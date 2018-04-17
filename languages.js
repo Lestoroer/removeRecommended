3d70010f7a106912e5ec99de0adfebc533a1d2c1
Sergey Alekseev <keny510@mail.ru>
2018-04-17 13:13:53 +0300

    new langiages

--- languages.js
+++ languages.js
@@ -1,23 +1,16 @@
-title = chrome.i18n.getMessage("extName");
+//title = chrome.i18n.getMessage("extName");
 
 function start() {
 	var elements = ['hideRecomRight', 'hideRecomHome', 'hideComment', 'hidePlayList', 'hideTranding'];
-	var css = ['.cntrRight', '.cntrHome', '.cntrComment', '.cntrPlayList', '.hideTranding'];
+
 	for (var i = 0; i < elements.length; i++) {
 		var message = chrome.i18n.getMessage(elements[i]);
 		changeHtml(message, elements[i]);
-		message = chrome.i18n.getMessage(elements[i]+'css');
-		changeCss(message, css[i]);
 	}
-  }
+}
 
 function changeHtml(message, element) {
 	document.getElementsByClassName(element)[0].innerHTML = message;
-}
-
-function changeCss(message, element) {
-	// console.log(element);
-	document.querySelector(element).style.margin =  message;
 }
 
 start();

a77b2f014405ea28a35b072c0103ceab16577256
ser <test@m.ru>
2018-05-07 23:30:20 +0500

    exactly all

--- extension/popup.js
+++ extension/popup.js
@@ -49,18 +49,17 @@ tabs.init();
 let switchers = {
 	data:{
 		'youtube':{
-			mine:true,
-			mine_recommen:true,
+			mine_recommen:false,
 			right:true,
-			comment:false,
-			playlist:false,
-			tranding:true,
-			videowall:true
+			comment:true,
+			playlist:true,
+			tranding:false,
+			videowall:false
 		},
 		'vk.com': {
 			vk_history:true,
 			vk_people_you_may_know:true,
-			vk_ads:true,
+			vk_ads:false,
             vk_menu_myprofile: true,
             vk_menu_news: true,
             vk_menu_messages: true,
@@ -72,11 +71,38 @@ let switchers = {
             vk_menu_games: true,
             vk_menu_market: true,
             vk_menu_bookmarks: true,
-            vk_menu_documents: true,
+			vk_menu_documents: true,
+			vk_menu_ads: true,
 		},
 		'facebook': {
 			fb_people_you_may_know:true,
-			fb_did_you_know:true
+			fb_menu_news_feed: true,
+			fb_menu_items_messenger: true,
+			fb_menu_groups: true,
+			fb_menu_pages: true,
+			fb_menu_events: true,
+			fb_menu_friend_lists: true,
+			fb_menu_on_this_day: true,
+			fb_menu_pages_feed: true,
+			fb_menu_photos: true,
+			fb_menu_find_friends: true,
+			fb_menu_games: true,
+			// fb_menu_suggest_edits: true,
+			fb_menu_offers: true,
+			fb_menu_live_video: true,
+			fb_menu_payment_history: true,
+			fb_menu_gaming_video: true,
+			fb_menu_buy_and_sell_groups: true,
+			fb_menu_create_a_frame: true,
+			fb_menu_discover_people: true,
+			fb_menu_crisis_response: true,
+			fb_menu_recommendations: true,
+			fb_menu_recent_ad_activity: true,
+			fb_menu_saved: true,
+			fb_menu_weather: true,
+			fb_menu_create_a_frame: true,
+			fb_menu_manage_apps: true,
+			fb_menu_insights: true,
 		}
 	},
 	dom: document.querySelectorAll('.checkbox'),
@@ -124,47 +150,28 @@ let switchers = {
 	}
 }
 
-let items_classes = ['.vk_items', '.fs_items'];
+let items_classes = ['vk_items', 'fs_items'];
 
 for (let i = 0; i < items_classes.length; i++) {
 	let item_class = items_classes[i];
-    js.listen(js.query(`.items${item_class}`), 'click', (event) => {
-        if (event.target.closest(`${item_class}`)) {
-            return js.toggleClass(document.querySelector(`.row_items_scroll${item_class}`), 'showed');
+    js.listen(js.query(`.items.${item_class}`), 'click', (event) => {
+        if (event.target.closest(`.${item_class}`)) {
+            return js.toggleClass(document.querySelector(`.row_items_scroll.${item_class}`), 'showed');
         }
     });
 }
 
-let items_pick_classes = ['.vk_items', '.fs_items'];
-
 function updatePickCount() {
+    for (let i = 0; i < items_classes.length; i++) {
+		let item_class = items_classes[i];
+		let item = js.query(`.row_items_scroll.${item_class}`);
+		let item_lenght = item.querySelectorAll('.checkbox[checked="true"]').length;
+		let item_lenght_max = item.querySelectorAll('.checkbox').length;
 
-    // for (let i = 0; i < items_pick_classes.length; i++) {
-    //     let item_class = items_classes[i];
-    //     js.listen(js.query(`.items${item_class}`), 'click', (event) => {
-    //         if (event.target.closest(`${item_class}`)) {
-    //             return js.toggleClass(document.querySelector(`.row_items_scroll${item_class}`), 'showed');
-    //         }
-    //     });
-    // }
-    //
-    // let dom_vk_menu_pick_count = document.getElementById('vk_menu_pick_count');
-    // let checkbox_picked = document.getElementsByClassName('vk_items')[0].querySelectorAll('.checkbox[checked="true"]');
-    // dom_vk_menu_pick_count.innerText = checkbox_picked.length;
+		js.get(`#${item_class}_pick_count`).innerText = item_lenght;
+		js.get(`#${item_class}_pick_count_max`).innerText = item_lenght_max;
+    }
 }
-
-// js.listen(js.queryAll('.items.superclass_space'), 'click', (event) => {
-// 	console.log(event.target);
-//     if (event.target.closest('.vk_items')) {
-//     	console.log(document.querySelector('.row_items_scroll.vk_items'));
-//         return js.toggleClass(document.querySelector('.row_items_scroll.vk_items'), 'showed');
-//     }
-//
-//     if (event.target.closest('.fs_items')) {
-//         return document.querySelector('.row_items_scroll.fs_items').toggle("showed");
-//     }
-// });
-
 
 
 

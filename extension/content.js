140c1f68b57b95b4f3d44f782a09fb8fca36c89c
max <wencorsman@gmail.com>
2018-05-08 23:39:57 +0500

    server data usr

--- extension/content.js
+++ extension/content.js
@@ -8,11 +8,13 @@ function loadCSS(file='smart_extensions_style') {
 }
 loadCSS();
 
+console.log(location.href);
+
 function start(data) {
     console.log('data', data);
 	chrome.storage.sync.get(function(item) {
         for (let i in item) {
-            if (i === 'sort') continue;
+            if (i === 'sort' || i === 'login') continue;
             if (location.href.includes(i)) { // site
                 let body = document.querySelector('html');
                 for (let k in item[i]) {
@@ -26,3 +28,17 @@ start();
 chrome.runtime.onMessage.addListener(function (data) {
 	start(data);
 });
+
+window.onload = function () {
+    if (location.href.includes('127.0.0.1')) {
+        let login = document.querySelector('[login]');
+        if (!login) return;
+        let user = login.innerHTML;
+        console.log(JSON.parse(data).response[0]);
+        chrome.storage.sync.get((data) => {
+            data.login = user;
+            chrome.storage.sync.set(data);
+        });
+    }
+}
+    
--- server/auth/vk.js
+++ server/auth/vk.js
@@ -20,23 +20,16 @@ module.exports = (app) => {
             // res.redirect(link);
     
             request(link, function (err, res2, body) {
-                console.log(body);
-                if (body === undefined) return console.log(arguments);
+                if (err) return console.log(err);
                 let data = JSON.parse(body);
-                let user_id = `v${data.user_id}`;
+                let user_id = data.user_id;
                 let access_token = data.access_token;
-                console.log(user_id, access_token);
-                res.end(`your id = ${user_id}`);
-                // mongo.updateById({ id: user_id }, { id: user_id, token: token }, function (err, rez) {
-                //     // console.log(rez.result);
-                //     users[user_id] = users[user_id] || {};
-                //     users[user_id].token = token;
-                //     let options = { maxAge: 1000 * 60 * 60 * 24 * 365 };
-                //     res.cookie('id', user_id, options);
-                //     res.cookie('token', token, options);
-                //     res.redirect('/');
-                //     console.log(users);
-                // });
+
+                request(`https://api.vk.com/method/users.get?user_ids=${user_id}&fields=photo_50,city,verified&access_token=${access_token}&v=5.74`, function(err, rez, body) {
+                    if (err) return console.log(err);
+                    res.send(`<div login>${body}</div>`);
+                });
+
             });
         } else {
             let link = `https://oauth.vk.com/authorize?client_id=${secret.vk.client_id}&response_type=code&display=mobile&redirect_uri=${secret.domen}/vk/`;
